1613fcd5e44cf990c53cf6938223f64164321db3
OMarHamdy <realomar2299@gmail.com>
2026-08-01 18:57:58 +0300

    This is V1

---
+++ lib/screens/test_drawer_youyube.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class TestDrawerYouyube extends StatelessWidget {
+  const TestDrawerYouyube({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Verve"),backgroundColor: Colors.blueAccent,),
+      
+      drawer: Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(child: Text("Verve")),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Home"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Profile"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("Settings"),
+              onTap: () {},
+            ),
+            Spacer(),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text("Logout"),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+      
+    );
+  }
+}
